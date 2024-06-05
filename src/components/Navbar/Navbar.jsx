c5ccb98e713cce0993da754ecd52b8978f6b97ae
Abhishek94Bire <salesagroprollp@gmail.com>
2024-06-05 13:58:18 +0530

    new commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,9 @@ export const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      
+      <a className={styles.title} href="/">
+        
+      </a>
       <div className={styles.menu}>
       <img
           className={styles.menuBtn}
